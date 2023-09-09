4a0224d0fbbb688e993e10116fcf85761df77562
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-09-09 20:25:19 +0530

    expense scripts

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,8 +1,12 @@
+# if we assign a name to commands , then it is a function
+
 # declare a function
 
 abc() {
   echo Hello world
+  echo Hello universe
+  echo Hello galaxy
 }
 
 # how to access a function
-abc
+abcf
