e8917b22fa5715e276b505c61d0a9a4d284ec350
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-09-10 08:47:57 +0530

    expense scripts

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,22 @@
 echo Hello world
+
+# there are total 6 colors in shell
+#red         - 31
+#green       - 32
+#yellow      - 33
+#blue        - 34
+#magenta     - 35
+#cyan        - 36
+
+# synatx of color print
+# echo -e "\e[COLmMESSAGE\[0m"
+# -e option to enable color
+# \e[COLM - to define which color
+# \e[0m -to disable color
+
+echo -e  "\e[31mred color\e[0m"
+echo -e  "\e[31mgreen color\e[0m"
+echo -e  "\e[31myellow color\e[0m"
+echo -e  "\e[31mblue color\e[0m"
+echo -e  "\e[31mmagenta color\e[0m"
+echo -e  "\e[31mcyan color\e[0m"
